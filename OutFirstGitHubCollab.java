a4d3ede374a01cc40478b56f5966c6ccf0c9bc05
Nova Diana <nopadr12@gmail.com>
2023-09-13 15:23:40 +0700

    tambahan anggota

--- OutFirstGitHubCollab.java
+++ OutFirstGitHubCollab.java
@@ -1,8 +1,8 @@
-public class OurFirstGitHubCollab{
-    public static void main (string[]args){
+public class OutFirstGitHubCollab{
+    public static void main (String[]args){
         System.out.println("ini ceritanya fitur pertama yang sudah berjalan dengan baik");
 
         System.out.println("ketua mulai mengembangkan fitur ke 2");
-        System.out.println("Tambahan dari anggota 1")
+        System.out.println("Tambahan dari anggota 12345");
     }
 }
