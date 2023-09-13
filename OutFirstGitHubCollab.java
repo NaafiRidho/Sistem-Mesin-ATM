2886cfa7832b928b43347c290289de7801ac8095
NaafiRidho <naafiridho0505@gmail.com>
2023-09-13 14:23:26 +0700

    tambahan

--- OutFirstGitHubCollab.java
+++ OutFirstGitHubCollab.java
@@ -1,8 +1,10 @@
-public class OurFirstGitHubCollab{
-    public static void main (string[]args){
+public class OutFirstGitHubCollab{
+    public static void main (String[]args){
         System.out.println("ini ceritanya fitur pertama yang sudah berjalan dengan baik");
 
         System.out.println("ketua mulai mengembangkan fitur ke 2");
-        System.out.println("Tambahan dari anggota 1")
+        System.out.println("Tambahan dari anggota 1");
+        System.out.println("Tambahan dari anggota 1");
+    }
     }
 }
