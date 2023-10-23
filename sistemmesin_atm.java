ca3ea2e2c9fdafda8bd5c755911a25b3c1da60c2
NaafiRidho <naafiridho0505@gmail.com>
2023-10-23 07:23:06 +0700

    looping sistem mesin atm

--- sistemmesin_atm.java
+++ sistemmesin_atm.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 
 public class sistemmesin_atm {
     public static void main(String[] args) {
-        String user, namaBank = "bank";
-        char operator;
+        String user, namaBank;
+        char operator, konfirmasi;
         double password, saldo = 5000000, tarikUang, tabunganAkhir, transfer, pajak, nomorRekening;
         Scanner sc = new Scanner(System.in);
 
@@ -13,56 +13,75 @@ public class sistemmesin_atm {
         System.out.println("========================================");
         System.out.println("masukkan user:");
         user = sc.nextLine();
+        user = sc.nextLine();
         System.out.println("masukkan password:");
         password = sc.nextDouble();
 
-        if (user.equalsIgnoreCase("naafi") && password == 123) {
-            System.out.println("1. cek saldo");
-            System.out.println("2.ambil uang");
-            System.out.println("3. transfer");
-        } else {
-            System.out.println("Username dan password salah");
-        }
-        // case
-        System.out.println("masukkan (1 2 3 4:)");
-        operator = sc.next().charAt(0);
-
-        switch (operator) {
-            case '1':
-                System.out.println("===========================");
-                System.out.println("       cek saldo anda      ");
-                System.out.println("      saldo anda" + saldo);
-                break;
-
-            case '2':
-                System.out.println("===========================");
-                System.out.println("     total ambil uang      ");
-                tarikUang = sc.nextDouble();
-                tabunganAkhir = saldo - tarikUang;
-                System.out.println("tabungan akhir anda:" + tabunganAkhir);
-                break;
-
-            case '3':
-            System.out.println("Masukkan Nama Bank");
-            namaBank = sc.nextLine();
-            namaBank = sc.nextLine();
-            
-            if (namaBank.equalsIgnoreCase("bank")) {
-                pajak = 0;
+        do {
+            if (user.equalsIgnoreCase("naafi") && password == 123) {
+                System.out.println("1. cek saldo");
+                System.out.println("2.ambil uang");
+                System.out.println("3. transfer");
             } else {
-                pajak = 2500;
-            }   
-            
+                System.out.println("Username dan password salah");
+            }
+            // case
+            System.out.println("masukkan (1 2 3 4:)");
+            operator = sc.next().charAt(0);
 
-                System.out.println("===========================");
-                System.out.println("  Masukkan Nomor rekening  ");
-                nomorRekening = sc.nextDouble();
-                System.out.println("Masukkan total transfer : ");
-                transfer = sc.nextDouble();
-                tabunganAkhir = saldo - transfer - pajak;
-                System.out.println("Tabungan Akhir Anda : " + tabunganAkhir);
-                break;
+            switch (operator) {
+                case '1':
+                    System.out.println("===========================");
+                    System.out.println("       cek saldo anda      ");
+                    System.out.println("      saldo anda" + saldo);
+                    break;
 
+                case '2':
+                    System.out.println("===========================");
+                    System.out.println("     total ambil uang      ");
+                    tarikUang = sc.nextDouble();
+                    tabunganAkhir = saldo - tarikUang;
+                    System.out.println("tabungan akhir anda:" + tabunganAkhir);
+                    break;
+
+                case '3':
+                    System.out.println("Pilih bank 1,2,3");
+                    namaBank = sc.nextLine();
+                    namaBank = sc.nextLine();
+                    switch (namaBank) {
+                        case "1":
+                            System.out.println("mandiri");
+                            break;
+                        case "2":
+                            System.out.println("bca");
+                            break;
+                        case "3":
+                            System.out.println("bri");
+
+                    }
+
+                    if (namaBank.equalsIgnoreCase("1")) {
+                        pajak = 0;
+                    } else {
+                        pajak = 2500;
+                    }
+
+                    System.out.println("===========================");
+                    System.out.println("  Masukkan Nomor rekening  ");
+                    nomorRekening = sc.nextDouble();
+                    System.out.println("Masukkan total transfer : ");
+                    transfer = sc.nextDouble();
+                    tabunganAkhir = saldo - transfer - pajak;
+                    System.out.println("Tabungan Akhir Anda : " + tabunganAkhir);
+                    break;
+            }
+            System.out.println("Apakah ingin kembali ke menu awal (Y/T)? ");
+            konfirmasi = sc.next().charAt(0);
+
+        } while (konfirmasi == 'y' || konfirmasi == 'Y');
+
+        if (konfirmasi == 't' || konfirmasi == 'T') {
+            System.out.println("Terima kasih");
         }
 
     }
